f4e054ddde845a973244a968f3e5dcc0cdc0c19b
gkpty <gkardonski@gmail.com>
2020-05-19 22:15:35 -0500

    the translate command is ready to get tested. need to rename the command to localize

--- src/commands/translate.js
+++ src/commands/translate.js
@@ -1,55 +1,76 @@
 const {Command, flags} = require('@oclif/command')
-const Translate = require('arjan-translate')
+const Translate = require('arjan-localize')
 const fs = require('fs')
+const ignorePaths = {
+  "node_modules":true,
+}
 
-function localizeAndTranslate(html, file, translation, name, from, to){
-  Translate.CreateLocale()  (html, name, from, to, function(err, data){
-    if(err) console.log(err)
-    else{
-      var origin_html = data.html;
-      fs.promises.writeFile(`locales/${from}/${from}.json`, JSON.stringify(data.locale));
-      fs.promises.writeFile(file, origin_html)
-      
-      //only execute if the destination language is provided
-      TranslateLocale(data.locale, name, from, to, data.size, function(err, data){
-        if(err) console.log(err)
-        else{
-          fs.promises.writeFile(`locales/${to}/${to}.json`, JSON.stringify(data));
-          TranslateHtml(origin_html, data)
-          .then((data)=> {
-            fs.promises.writeFile(translation, data)
-            console.log('All Done!')
-          })
-          .catch(err => console.log(err))
+function scanFolder(currentDirPath, callback) {
+  return new Promise((resolve, reject) => {
+    let dirArr = fs.readdirSync(currentDirPath)
+    for(let name of dirArr){
+      console.log(name)
+      var filePath = path.join(currentDirPath, name);
+      var stat = fs.statSync(filePath);
+      if (stat.isFile()){
+        let ext = name.substr(name.lastIndexOf("."), name.length)
+        if(ext === "html") callback(filePath, stat);
+      }
+      else if (stat.isDirectory()) {
+        if(ignorePaths[filePath]) console.log("ignoring ", fileSubPath)
+        else {
+          if(!fs.existsSync(`output/${fileSubPath}`)) fs.mkdirSync(`output/${fileSubPath}`)
+          scanFolder(filePath, callback);
         }
-      })
-
-
+      }
     }
+    resolve(name)
+  });
+}
+
+function localizeAndTranslate(html, filePath, from, to){
+  return new Promise(async (resolve, reject) => {
+    let data = await Translate.CreateLocale(html).catch(err => reject(err))
+    var origin_html = data.html
+    let destname = to+'/'+filePath;
+    let localename = filePath.substr(filePath.lastIndexOf("."), filePath.length).replace('/', '_')
+    await fs.promises.writeFile(`locales/${from}/${localename}.json`, JSON.stringify(data.locale)).catch(err => reject(err));
+    await fs.promises.writeFile(filePath, origin_html).catch(err => reject(err));
+    if(to){
+      let translatedLocale = await Translate.TranslateLocale(data.locale, from, to, data.size).catch(err => reject(err));
+      await fs.promises.writeFile(`locales/${to}/${localename}.json`, JSON.stringify(translatedLocale)).catch(err => reject(err))
+      let translatedHtml = await TranslateHtml(origin_html, translatedLocale).catch(err => reject(err))
+      if(localename === from) destname = to+'.html'
+      else if(filePath.split('/')[0] === from) destname = to+filePath.split('/', 2)[1]  
+      await fs.promises.writeFile(destname, translatedHtml).then(resolve(true)).catch(err => reject(err))
+    }
+    else resolve(true)
   })
 }
 
-function translateFunction(flags, args){
-  let html = await fs.promises.readFile(args.filename)
-  let json = await fs.promises.readFile(`./locales/${args.from}/${args.filemname}`)
-  if(flags.create) localizeAndTranslate()
-  else if(flags.update && flags.backwards) Translate.CreateLocale()
-  else if(flags.update) Translate.TranslateHtml(html, json)
-  else if(flags.import) Translate.importCsv()
-  if(flags.export) Translate.exportCsv()
+async function localizeTranslateFile(flags, args){
+  let fileContent = await fs.promises.readFile(args.filename, 'utf8')
+  let json = await fs.promises.readFile(`./locales/${args.from}/${args.filename}`, 'utf8')
+  if(flags.create) await localizeAndTranslate(fileContent, args.filename, args.from, args.to)
+  else if(flags.update && flags.backwards) await Translate.CreateLocale(fileContent)
+  else if(flags.update) await Translate.TranslateHtml(fileContent, json)
+  else if(flags.import) Translate.importCsv(args.from, fileContent)
+  if(flags.export) {
+    Translate.exportCsv(args.from, json)
+    if(args.to) Translate.exportCsv(args.to, json)
+  }
 }
-
 
 class TranslateCommand extends Command {
   async run() {
     const {args, flags} = this.parse(TranslateCommand)
     if(args.filename === 'all'){
-      //recurse for all file
-      //for each html file 
-      //args.filename = filename
-      //translateFunction(flags, args)
+      scanFolder('./', (filePath, stat)=> {
+        args.filename = filePath
+        localizeTranslateFile(flags, args)
+      })
     }
-    else translateFunction(flags, args)
+    else localizeTranslateFile(flags, args)
   }
 }
 
@@ -59,14 +80,9 @@ Extra documentation goes here
 `
 TranslateCommand.args = [
   {
-    name: 'sitename',
-    required: true,
-    description: 'name of the site - must be unique.'
-  },
-  {
     name: 'filename',
     required: true,
-    description: 'name of the file you want to translate (only html files accepted) - The default is all.',
+    description: 'name of the file you want to translate -only html files accepted. Use all to translate all of your html files (default).',
     default: 'all'
   },
   {
