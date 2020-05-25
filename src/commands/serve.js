84a0bce68266378bef8c02a1f178f5d1c80d8d15
gkpty <gkardonski@gmail.com>
2020-05-25 12:43:20 -0500

    added option to ingore a default flags that causes an error warning --disable-setuid-sandbox

--- src/commands/serve.js
+++ src/commands/serve.js
@@ -1,6 +1,7 @@
 const {Command, flags} = require('@oclif/command')
 var nodemon = require('nodemon');
 const chromeLauncher = require('chrome-launcher');
+const newFlags = chromeLauncher.Launcher.defaultFlags().filter(flag => flag !== '--disable-setuid-sandbox')
 
 nodemon({
   script: 'app.js',
@@ -10,7 +11,11 @@ nodemon({
 class ServeCommand extends Command {
   async run() {
     const {flags} = this.parse(ServeCommand)
-    let chrome = await chromeLauncher.launch({startingUrl: 'http://localhost:8080/index.html'})
+    let chrome = await chromeLauncher.launch({
+      ignoreDefaultFlags: true,
+      chromeFlags: newFlags,
+      startingUrl: 'http://localhost:8080/index.html'
+    })
     nodemon.on('start', function () {
       console.log('App has started');
     }).on('quit', function () {
@@ -19,7 +24,7 @@ class ServeCommand extends Command {
     }).on('restart', function (files) {
       chromeLauncher.killAll()
       .then(()=>chromeLauncher.launch({startingUrl: 'http://localhost:8080/index.html'}))
-      console.log('App restarted due to: ', files);
+      //console.log('App restarted due to: ', files);
       console.log('FILEEEEEEEEEEEEEEEE ', files[0])
     });
   }
