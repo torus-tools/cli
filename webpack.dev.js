4bf9330454c3729523fd9d698e6152e7f68bcca0
gkpty <gkardonski@gmail.com>
2020-06-12 01:59:59 -0500

    got the index to load.. need to automatically recognize all html files in the direcotry and add to the webpack config..

---
+++ webpack.dev.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+
+  // https://webpack.js.org/concepts/entry-points/#multi-page-application
+  entry: {
+    index: './src/webpack.js'
+  },
+
+  // https://webpack.js.org/configuration/dev-server/
+  devServer: {
+    port: 8080
+  },
+
+  // https://webpack.js.org/concepts/plugins/
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './en/index.html',
+      inject: true,
+      chunks: ['index'],
+      filename: 'index.html'
+    })
+  ]
+};
