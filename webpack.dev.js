8a16d0422c8fa0fe76b654f7bdba000b87e4d2de
gkpty <gkardonski@gmail.com>
2020-06-13 15:04:03 -0500

    changing some stuff

--- webpack.dev.js
+++ webpack.dev.js
@@ -40,6 +40,7 @@ const getEntries =async()=>{
     //console.log(getAltName(filePath))
     entries[getAltName(filePath)] = './lib/'+filePath.substr(0,filePath.lastIndexOf('.')) + '.js'
   }
+  console.log(entries)
   return entries;
 }
 
@@ -74,10 +75,48 @@ module.exports = {
   plugins
 };
 
-/* 
-new HtmlWebpackPlugin({
-  template: './es/index.html',
-  inject: true,
-  chunks: ['es_index'],
-  filename: 'index.html'
-}) */
+
+
+/* module.exports = {
+
+  // https://webpack.js.org/concepts/entry-points/#multi-page-application
+  entry: {
+    index: './lib/index.js',
+    localize: './lib/localize.js',
+    es_audit: './lib/es/audit.js',
+    es_deploy: './lib/es/optimize.js'
+  },
+
+  // https://webpack.js.org/configuration/dev-server/
+  devServer: {
+    port: 8080
+  },
+
+  // https://webpack.js.org/concepts/plugins/
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      inject: true,
+      chunks: ['index'],
+      filename: 'index.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './localize.html',
+      inject: true,
+      chunks: ['localize'],
+      filename: 'localize.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './es/audit.html',
+      inject: true,
+      chunks: ['es_audit'],
+      filename: 'audit.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './es/deploy.html',
+      inject: true,
+      chunks: ['es_deploy'],
+      filename: 'deploy.html'
+    })
+  ]
+}; */
